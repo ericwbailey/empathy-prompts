a60e26735700ec8790fec3d331b64d182f6496ee
Eric Bailey <eric.w.bailey@gmail.com>
2017-05-07 17:20:56 -0400

    Updated URL and project description

--- tasks/sitemap.js
+++ tasks/sitemap.js
@@ -16,7 +16,7 @@ gulp.task('build-sitemap', help.sitemap.build, function() {
         read: false
     })
     .pipe(sitemap({
-        siteUrl: 'https://updateme.com'
+        siteUrl: 'https://empathyprompts.net.com'
     }))
     .pipe(gulp.dest(paths.sitemap.build.dest));
 });
